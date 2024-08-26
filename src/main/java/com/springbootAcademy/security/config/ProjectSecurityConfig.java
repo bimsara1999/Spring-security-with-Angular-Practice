4bcb307491756948ce9ff8ebfa8a8ca0f160ef69
bimsara1999 <blankanatha@gmail.com>
2024-08-26 14:48:59 +0545

    User entity

--- src/main/java/com/springbootAcademy/security/config/ProjectSecurityConfig.java
+++ src/main/java/com/springbootAcademy/security/config/ProjectSecurityConfig.java
@@ -3,7 +3,17 @@ package com.springbootAcademy.security.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class ProjectSecurityConfig {
@@ -11,10 +21,30 @@ public class ProjectSecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests();
-                //.anyRequest("/my-account").authenticated()
-               // .anyRequest("/my-notices").permitAll()
+               // .anyRequest("api/v1/account/my-account" , "api/v1/loan/my-loan").authenticated()
+              //  .anyRequest("api/v1/notice/my-notices").permitAll()
                // .and().formLogin().and().httpBasic();
 
-return http.build();
+        return http.build();
+    }
+/*
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService(){
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("1234")
+                .authorities("admin")
+                .build();
+        return new InMemoryUserDetailsManager(admin);
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(DataSource  dataSource){
+        return  new JdbcUserDetailsManager(dataSource);
+    }
+*/
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
---
+++ src/main/java/com/springbootAcademy/security/model/User.java
@@ -0,0 +1,7 @@
+package com.springbootAcademy.security.model;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class User {
+}
